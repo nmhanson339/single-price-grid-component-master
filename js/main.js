10ed963ed56ccb9f37d05a9f7f73d5a011910a7f
nmhanson339 <normh2048@gmail.com>
2021-03-25 17:27:22 -0400

    Add styles and script folders.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*
+ // Description of file:
+ // 
+*/
+
+// - - - - - HTML ELEMENTS - - - - -
+
+// - - - - - CONSTANTS AND VARIABLES - - - - -
+
+// - - - - - EVENT LISTENERS - - - - -
+
+// - - - - - FUNCTIONS - - - - -
